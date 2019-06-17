084980120a0edd804dae7c25222ebb00b7a5aab4
krushang <krushang@krushangpatel>
2019-06-17 00:41:32 -0400

    arithmetic file done

---
+++ bash/arithmetic-demo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# this script demonstrates doing arithmetic
+
+# Improve this script by asking the user to supply the two numbers
+# Improve this script by demonstrating using subtraction and multiplication
+# Improve this script by demonstrating the modulus operation
+#   - the output should look something like:
+#   - first divided by second gives X with a remainder of Y
+# Improve this script by calculating and displaying the first number raised to the power of the second number
+
+#firstnum=5
+#secondnum=2
+read -p "Give me 1st number::" firstnum
+read -p "Give me 2nd number::" secondnum
+#===============================================
+sum=$((firstnum + secondnum))
+subtraction=$((firstnum - secondnum))
+dividend=$((firstnum / secondnum))
+multiplication=$((firstnum * secondnum))
+fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+power=$((firstnum ** secondnum))
+
+cat <<EOF
+$firstnum plus $secondnum is:: $sum
+$firstnum subtraction $secondnum is:: $subtraction
+$firstnum multiplication $secondnum is:: $multiplication
+$firstnum power $secondnum is:: $power
+$firstnum divided by $secondnum is:: $dividend
+  - More precisely, it is:: $fpdividend
+EOF
