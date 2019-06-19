68b5f178a68dcd190dcd7d60161f6c3c8d981a77
krushang <krushang@krushangpatel>
2019-06-19 10:07:31 -0400

    update

--- bash/login-welcome.sh
+++ bash/login-welcome.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-#
-# This script produces the dynamic welcome message
-# it should look like
-#   Welcome to planet hostname, title name!
-
-# Improve this script by using the value in the automatic variable $USER for the name
-# Improve this script by adding some time and day of the week information to the welcome message
-#   Use a format like this:
-#   It is HH:MM AM on weekday.
-
-###############
-# Variables   #
-###############
-currenttimeandday=`date +"%I:%M %p %a"`
-title="loading"
-User="Krushang"
+titles=("one" "two" "three")
+number=$(( ${#titles[@]}))
+random=$(( RANDOM % number))
+time=$(date +'%A, %I:%M %p')
 hostname=$(hostname)
-echo Current Time is: ${currenttimeandday}
+weekday=$(date +%u)
+variable=$(cat <<EOF
+Welcome to planet $hostname, ${titles[$random]} $USER!
 
-###############
-# Main        #
-###############
+$(if [ "$weekday" = "6" ] || [ "$weekday" = "7" ]
+then
+   echo "It is $time on Weekend."
+else
+   echo "It is $time on Weekday."
+fi
+)
+EOF
+)
 cat <<EOF
-
-Welcome to planet $hostname, "$title $User!"
-
+$(cowsay$variable)
 EOF
